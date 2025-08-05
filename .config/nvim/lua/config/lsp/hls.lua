cd9e44a985c65e3974bd9b41165d97d8f5cdc5b6
Eliton Machado <elitonmachadod200@gmail.com>
2025-08-05 17:55:42 -0300

    Feat: remove haskell lsp

--- .config/nvim/lua/config/lsp/hls.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    require 'lspconfig'.hls.setup {
-        settings = {
-            haskell = {
-                formattingProvider = 'fourmolu'
-            }
-        }
-    }
-end
