89a7194b70fbfb8d8d35e1a494a26311a65c5a57
Eliton Machado <elitonmachadod200@gmail.com>
2025-08-07 22:23:19 -0300

    Feat: telescope file picker toggle hidden

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,13 +7,109 @@ return {
     },
     keys = {
         -- find
-        { '<leader>fb', '<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>', desc = 'Recent Buffers' },
-        { '<leader>ff', '<cmd>Telescope find_files<cr>',                               desc = 'Find Files (Root Dir)' },
+        { '<leader>fb', '<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>',  desc = 'Recent Buffers' },
+        {
+            '<leader>ff',
+            function()
+                local builtin = require('telescope.builtin')
+                local hidden = vim.g.telescope_show_hidden_files == true
+                builtin.find_files({
+                    hidden = hidden,
+                })
+            end,
+            desc = 'Find Files',
+        },
+        {
+            '<leader>fs',
+            function()
+                local builtin = require('telescope.builtin')
+                local hidden = vim.g.telescope_show_hidden_files == true
+                builtin.live_grep({
+                    additional_args = function()
+                        if hidden then
+                            return { '--hidden' }
+                        end
+                        return {}
+                    end,
+                })
+            end,
+            desc = 'Find String',
+        },
         -- undo buffer
         { '<leader>fu', '<cmd>Telescope undo<cr>', desc = 'Undo' }
     },
     config = function()
-        require('telescope').setup({})
-        require('telescope').load_extension('undo')
+        local telescope = require('telescope')
+        local builtin = require('telescope.builtin')
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        -- Persisted session toggle state
+        if vim.g.telescope_show_hidden_files == nil then
+            vim.g.telescope_show_hidden_files = false
+        end
+
+        local function get_hidden_state()
+            return vim.g.telescope_show_hidden_files == true
+        end
+
+        local function relaunch_find_files_with_hidden(prompt_bufnr)
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local current_text = action_state.get_current_line()
+            local cwd = picker and picker.cwd or vim.loop.cwd()
+            actions.close(prompt_bufnr)
+            vim.g.telescope_show_hidden_files = not get_hidden_state()
+            builtin.find_files({
+                cwd = cwd,
+                hidden = get_hidden_state(),
+                default_text = current_text,
+            })
+        end
+
+        local function relaunch_live_grep_with_hidden(prompt_bufnr)
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local current_text = action_state.get_current_line()
+            local cwd = picker and picker.cwd or vim.loop.cwd()
+            actions.close(prompt_bufnr)
+            vim.g.telescope_show_hidden_files = not get_hidden_state()
+            builtin.live_grep({
+                cwd = cwd,
+                additional_args = function()
+                    if get_hidden_state() then
+                        return { "--hidden", "--glob", "!.git/" }
+                    else
+                        return {}
+                    end
+                end,
+                default_text = current_text,
+            })
+        end
+
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = { ".git/" },
+                mappings = {
+                    i = {},
+                    n = {},
+                },
+            },
+            pickers = {
+                find_files = {
+                    mappings = {
+                        i = {
+                            ["<C-.>"] = relaunch_find_files_with_hidden,
+                        },
+                    },
+                },
+                live_grep = {
+                    mappings = {
+                        i = {
+                            ["<C-.>"] = relaunch_live_grep_with_hidden,
+                        },
+                    },
+                },
+            },
+        })
+        telescope.load_extension('undo')
     end
 }
