d4c9d4f2fad0b8fe2fb3d7bbdd9e095bf4faf667
Eliton Machado <elitonmachadod200@gmail.com>
2025-08-22 17:52:28 -0300

    Feat: better ruby lsp config

--- .config/nvim/lua/config/lsp/ruby_lsp.lua
+++ .config/nvim/lua/config/lsp/ruby_lsp.lua
@@ -1,4 +1,13 @@
 ---@type vim.lsp.Config
 return {
     cmd = { vim.fn.expand('~/.asdf/shims/ruby-lsp') },
+    filetypes = { 'ruby' },
+    root_markers = { 'Gemfile', '.git' },
+    init_options = {
+        addonSettings = {
+            ['Ruby LSP Rails'] = {
+                enablePendingMigrationsPrompt = false,
+            },
+        },
+    },
 }
